f05bdf6be9d224973591b9e00eb3f325fd9c1952
maximdave <enoragbondavid35@gmail.com>
2022-11-22 09:39:39 +0100

    reponsive nav

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -44,13 +44,13 @@ const Experience = () => {
         whileInView={{ y: [20, 0], opacity: [0, 1] }}
         transition={{ duration: 0.9 }}
         key={1}
-        className=" pt-16 w-homePageWidth grid place-items-center "
+        className="pt-4 sm:pt-16 w-homePageWidth grid place-items-center "
       >
-        <div className="flex gap-24">
-          <section className="pt-24">
+        <div className="block sm:flex gap-24 pl-8 sm:pl-24">
+          <section className="pt-0 sm:pt-24">
             <div>
               <div>
-                <h1 className="text-6xl font-bold tracking-wide leading-relaxed">
+                <h1 className="text-4xl  sm:text-6xl font-bold tracking-wide leading-relaxed">
                   {currentInfo.Title}
                   <br /> {currentInfo.break}
                 </h1>
@@ -60,7 +60,7 @@ const Experience = () => {
                 </p>
               </div>
 
-              <button className="border-2 border-green mt-4 py-3 px-8 rounded-lg cursor-pointer bg-green hover:bg-bg-bg-[#19643B]  text-white duration-300">
+              <button className="invisible md:visible border-2 border-green mt-4 py-3 px-8 rounded-lg cursor-pointer bg-green hover:bg-bg-bg-[#19643B]  text-white duration-300">
                 Chat with our team
               </button>
             </div>
@@ -73,17 +73,24 @@ const Experience = () => {
               <img src={homeImg} alt="home img" className=" w-full h-full " />
             </div>
           </section>
+
+          <button className="visible md:invisible border-2 border-green mt-8 py-3 px-8 rounded-lg cursor-pointer bg-green hover:bg-bg-bg-[#19643B]  text-white duration-300">
+            Chat with our team
+          </button>
         </div>
-        <section className=" bg-white px-8 mt-24 rounded-lg  w-[45%]  shadow-lg flex gap-8 justify-center items-center  ">
-          <div className=" font-bold border-r-2 ">
+        <div className="visible md:invisible justify-center items-center font-bold border-r-2 w-44 mt-12">
             <p>Our clients</p>
           </div>
-          <div className="pl-4 flex gap-24 justify-center items-center  ">
+        <section className=" bg-white px-8 mt-1 sm:mt-24 rounded-lg w-[85%] sm:w-[45%]  shadow-lg flex gap-8 justify-center items-center  ">
+          <div className="hidden md:visible font-bold md:border-r-2 w-44">
+            <p className="">Our clients</p>
+          </div>
+          <div className="md:pl-4 flex gap-12 md:gap-24 justify-center items-center  ">
             <div>
               <img
                 src={image1}
                 alt="logo"
-                className=" duration-500 pt-4 cursor-pointer w-32"
+                className=" duration-500  cursor-pointer w-32"
               />
             </div>
             <div>
@@ -97,7 +104,7 @@ const Experience = () => {
               <img
                 src={image3}
                 alt="logo"
-                className=" duration-500  pt-4 cursor-pointer w-32"
+                className=" duration-500 cursor-pointer w-32"
               />
             </div>
           </div>
